6a9e820a3bcc01cc196e0c9d78584f1dfd1686a7
Patange Pradnya Prasad (SX/EMS2) <ptp2kor@bosch.com>
2022-06-24 00:47:16 +0530

    alignment test cases

--- misaligned/Misaligned.java
+++ misaligned/Misaligned.java
@@ -1,3 +1,6 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Misaligned {
     static int printColorMap() {
         String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -10,9 +13,50 @@ public class Misaligned {
         }
         return i * j;
     }
+    
+    
+    static int printexpectedColorMap() {
+        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+        int i = 0, j = 0;
+        for(i = 0; i < 5; i++) {
+            for(j = 0; j < 5; j++) {
+                System.out.printf("%d | %10s | %10s\n", i * 5 + j, majorColors[i], minorColors[i]);
+            }
+        }
+        return i * j;
+    }
+    
     public static void main(String[] args) { 
         int result = printColorMap();
         assert(result == 25);
+        
+        // Create a stream to hold the actual output
+        ByteArrayOutputStream actualOutputStream = new ByteArrayOutputStream();
+        PrintStream actualPrintStream = new PrintStream(actualOutputStream);
+        
+        // Create a stream to hold the expected output
+        ByteArrayOutputStream expectedOutputStream = new ByteArrayOutputStream();
+        PrintStream expectedPrintStream = new PrintStream(expectedOutputStream);
+
+        PrintStream oldPrintStream  = System.out;
+        System.setOut(actualPrintStream);
+        printColorMap();        
+        
+        System.out.flush();       
+        System.setOut(expectedPrintStream);
+        printexpectedColorMap();
+        
+        System.out.flush();
+        System.setOut(oldPrintStream);
+
+        System.out.println("actual: \n" + actualOutputStream.toString());
+        System.out.println("expected: \n" + expectedOutputStream.toString());
+        
+        assert(actualOutputStream.toString()==expectedOutputStream.toString());       
+        
         System.out.println("All is well (maybe!)");
     }
+
+
 }
