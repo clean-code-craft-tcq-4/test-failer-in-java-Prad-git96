d9f34f19ac067cee983e9ae1234ca3f7e72d9952
Patange Pradnya Prasad (SX/EMS2) <ptp2kor@bosch.com>
2022-06-24 08:44:11 +0530

    misaligned test cases

--- misaligned/Misaligned.java
+++ misaligned/Misaligned.java
@@ -2,9 +2,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class Misaligned {
+
+   static String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+   static String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
     static int printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
@@ -16,8 +18,7 @@ public class Misaligned {
     
     
     static void printexpectedColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+       
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
